1a76977682f9e90d14a5dac36ad0ec1732b87b50
Santhosh Krishnan <imskrishnan@gmail.com>
2021-08-23 23:31:40 -0400

    refactor slice methods

--- arrays-01-starting-code/array-slice-method.js
+++ arrays-01-starting-code/array-slice-method.js
@@ -39,16 +39,3 @@ const shoppingCartConcat1 = shoppingCart.concat(
 );
 console.log(shoppingCartConcat1);
 console.log(shoppingCart);
-
-// indexOf - returns the first occurence of a value in the array or -1 if the element is not present.
-
-const cars = ['toyota', 'tesla', 'covette', 'porsche', 'honda', 'covette'];
-console.log(cars.indexOf('tesla'));
-
-// lastIndexOf() - returns the last occurence of a particular element in the array or returns -1 if none was found.
-console.log(cars.lastIndexOf('covette'));
-
-// this doesnt work for reference values like objects
-// here it doesnt work since eventhough the values passed into the indexOf looks similar to the data available in the personData array they are having different storage locations in the heap memory
-const personData = [{ name: 'Santhosh' }, { name: 'Virat' }];
-console.log(personData.indexOf({ name: 'Santhosh' }));
